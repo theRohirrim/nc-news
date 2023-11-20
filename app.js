e96cd90b58b48e22ca911e657b0e24dbae51d533
Josh Holt <jholt@Stephanie-2.local>
2023-11-20 16:05:36 +0000

    includes an extra require into app.js that was needed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getTopics, getEndpoints, getArticles } = require("./controllers/topics.controller");
+const { getTopics, getEndpoints, getArticles, getArticleById } = require("./controllers/topics.controller");
 const { handlePsqlError, handleCustomError, handleServerError } = require("./errors");
 
 const app = express();
