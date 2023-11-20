1f844993976e2961df57d777caa65751120db65c
Josh Holt <jholt@Stephanie-2.local>
2023-11-20 10:47:26 +0000

    add all happy path tests and completes get all topics feature. Sad path testing cannot be done until queries are introduced as no content found cannot be reasonably mocked and bad requests will not activate the endpoint in which to be tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -17,4 +17,40 @@ describe("GET /api/topics", () => {
     test("200: Get the correct status code", () => {
         return request(app).get("/api/topics").expect(200);
     })
+
+    test("200: Check returned result is an array", () => {
+        return request(app)
+          .get("/api/topics")
+          .expect(200)
+          .then((res) => {
+            expect(Array.isArray(res.body.topics)).toBe(true);
+        });
+    });
+    
+    test("200: Check the returned results keys are correct", () => {
+    return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then((res) => {
+        res.body.topics.forEach((topic) => {
+            expect(topic).toMatchObject({
+            slug: expect.any(String),
+            description: expect.any(String)
+            });
+        });
+        });
+    });
+
+    test("200: Topics table should have correct number of rows", () => {
+        return request(app)
+          .get("/api/topics")
+          .expect(200)
+          .then((res) => {
+            expect(res.body.topics.length).toBe(3);
+        });
+    });
+
+    /* Sad path tests cannot be reasonably mocked until queries
+    are introduced as a bad request at this point cannot activate
+    the endpoint */
 })
--- connectors/topics.connector.js
+++ connectors/topics.connector.js
@@ -1,9 +1,9 @@
 const { selectTopics } = require("../models/topics.model")
 
 exports.getTopics = (req, res, next) => {
-    console.log("HERE IN CONTROLLER")
     return selectTopics()
     .then((topics) => {
-        res.status(200).send(topics);
+        res.status(200).send({topics});
     })
+    .catch(next);
 }
