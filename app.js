38730d19f7ce9f3e3960e9d1c529c47cd916e08d
Josh Holt <jholt@Stephanie-2.local>
2023-11-20 11:00:54 +0000

    removes unnecessary app.use(express.json()) until needed. Merging now

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { getTopics } = require("./connectors/topics.connector");
 
 const app = express();
-app.use(express.json())
 
 app.get("/api/topics", getTopics);
 
